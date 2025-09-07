64e549320707b7eaa6cac2e003c5ece746fcc9f7
truptibhat67 <truptibhat67@gmail.com>
2025-09-07 18:52:23 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 1000 
+  plugins: [react()],
+  optimizeDeps: {
+    include: ["react-router", "react-router-dom"]
   }
 })
