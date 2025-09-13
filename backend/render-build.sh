e8d8bddcb8760612546e1d14787f63aef42c6bfe
truptibhat67 <truptibhat67@gmail.com>
2025-09-13 22:49:12 +0530

    commit

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Make sure pip, setuptools, and wheel are up-to-date
-python -m ensurepip --upgrade
-python -m pip install --upgrade pip setuptools wheel
+python3.11 -m ensurepip --upgrade
+python3.11 -m pip install --upgrade pip setuptools wheel
 
 # Optional: add Rust if any package needs compilation
 # curl https://sh.rustup.rs -sSf | sh -s -- -y
 # export PATH="$HOME/.cargo/bin:$PATH"
 
 # Install project dependencies
-python -m pip install -r requirements.txt
+python3.11 -m pip install -r requirements.txt
